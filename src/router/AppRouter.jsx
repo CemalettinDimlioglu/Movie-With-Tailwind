65c4f84a6ed2b99df991d7ddd787463055f3953f
CMLTTN <cemaldim@gmail.com>
2023-08-03 16:18:38 +0300

    BrowserRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,22 @@
-import React from "react";
-
-const AppRouter = () => {
-  return <div>AppRouter</div>;
-};
-
-export default AppRouter;
+ import React from "react";
+ import { BrowserRouter, Route, Routes } from "react-router-dom";
+ import Login from "./../pages/Login";
+ import Main from "../pages/Main";
+ import Navbar from "../components/Navbar";
+ import Register from "../pages/Register";
+ import MovieDetail from "../pages/MovieDetail";
+ //Routes yerine switch //
+ const AppRouter = () => {
+   return (
+     <BrowserRouter>
+       <Navbar />
+       <Routes>
+         <Route path="/" element={<Main />} />
+         <Route path="/login" element={<Login />} />
+         <Route path="/register" element={<Register />} />
+         <Route path="/details/:id" element={<MovieDetail />} />
+       </Routes>
+     </BrowserRouter>
+   );
+ };
+ export default AppRouter;
